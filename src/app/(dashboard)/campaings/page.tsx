84fdd6d17d6581e12aa992858b0f14b4cd9bd9ab
Carlos Monsalve <carlos.monsalve@gelt.com>
2024-04-25 13:35:07 +0200

    Configurations init

---
+++ src/app/(dashboard)/campaings/page.tsx
@@ -0,0 +1,10 @@
+import Filters from '@components/campaings/filters/Filters'
+
+const UseFilters = () => {
+
+  return (
+    <Filters><div>Filters</div></Filters>
+  )
+}
+
+export default UseFilters
---
+++ src/components/campaings/filters/Filters.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+//import { LayoutContent } from "@layouts/utils/layoutClasses";
+
+
+export default function Filters() {
+  return
+  (
+    <div >
+      <p>Customers Page</p>
+    </div>
+  )
+}
