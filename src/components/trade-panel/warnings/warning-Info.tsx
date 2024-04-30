7f085ee0cc6ba8463ec453608a299d159b81d1f4
Carlos Monsalve <carlos.monsalve@gelt.com>
2024-04-30 09:27:26 +0200

    new configurations

--- src/components/trade-panel/warnings/warning-Info.tsx
+++ src/components/trade-panel/warnings/warning-Info.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
+import type { Theme } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core';
 import { Box, Divider, Typography, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }),
+);
 
 export default function WarningInfo() {
-
+  const classes = useStyles();
 
   return (
     <Box>
       <Box display={'flex'}>
-        <Box
-          width={600}
-          pt={10}
-          pb={12}
-        >
+        <Box width={600} pt={10} pb={12} >
           <TextField
             disabled
             id="filled-disabled"
@@ -42,7 +51,7 @@ export default function WarningInfo() {
       <Box
         display="flex"
       >
-        <Box>
+        {/* <Box>
           <TextField
             disabled
             id="filled-disabled"
@@ -116,7 +125,22 @@ export default function WarningInfo() {
             defaultValue="58%"
             variant="filled"
           />
-        </Box>
+        </Box>*/}
+
+        <div className={classes.root}>
+          <Typography variant='h5' pt={3} pb={3} >
+            {`Acción 1 `}
+          </Typography>
+          {/* <Button variant="contained" color="success" href="/">Buscar</Button> */}
+          <Button variant="outlined" color="primary">REVISAR TICKETS</Button>
+          <Button variant="outlined" color="primary">REVISAR ENSEÑAS ONLINE</Button>
+          <Typography variant='h5' pt={3} pb={3} >
+            {`Acción 2 `}
+          </Typography>
+          <Button variant="outlined" color="primary">MODIFICAR MARGEN</Button>
+          <Button variant="outlined" color="primary">AÑADIR EXCLUSIÓN</Button>
+
+        </div>
       </Box>
     </Box>
   );
